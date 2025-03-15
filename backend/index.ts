5466bb1cec613310354ba112e30721a3c076d267
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-03-15 16:30:01 +0530

    removed cors type

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import express, { Request } from "express";
+import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import * as dotenv from "dotenv";
@@ -14,7 +14,7 @@ app.use(
   })
 );
 app.use(
-  cors<Request>({
+  cors({
     origin: "https://helioweb-bingo.vercel.app/",
   })
 );
